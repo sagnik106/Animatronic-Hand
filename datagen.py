8ed23255df30a12384ba1cbba30dd7a65f571b71
Sagnik Sarkar <sagnik106@gmail.com>
2019-06-13 23:29:31 +0530

    New model

---
+++ datagen.py
@@ -0,0 +1,59 @@
+#Libraries
+import cv2
+import numpy as np
+import os
+import time
+
+#Generator
+#Modify or change the following lines of code for a category
+"""
+Category Template:
+    elif c==ord(''):
+        f=open("data/",'r')
+        i=int(f.read())
+        f.close()
+        t=str(i)+'.jpg'
+        cv2.imwrite(os.path.join("data/",t),frame)
+        f=open("data/",'w')
+        f.write("%d"%(i+1))
+        f.close()
+"""
+
+cap=cv2.VideoCapture(0)
+while(True):
+    ret,frame=cap.read()
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frame=frame[:,81:561]
+    cv2.imshow("CAMERA",frame)
+    c=cv2.waitKey(1)
+    if c==ord('r'):
+        f=open("data/rock/rock.txt",'r')
+        i=int(f.read())
+        f.close()
+        t=str(i)+'.jpg'
+        cv2.imwrite(os.path.join("data/rock",t),frame)
+        f=open("data/rock/rock.txt",'w')
+        f.write("%d"%(i+1))
+        f.close()
+    elif c==ord('p'):
+        f=open("data/paper/paper.txt",'r')
+        i=int(f.read())
+        f.close()
+        t=str(i)+'.jpg'
+        cv2.imwrite(os.path.join("data/paper",t),frame)
+        f=open("data/paper/paper.txt",'w')
+        f.write("%d"%(i+1))
+        f.close()
+    elif c==ord('s'):
+        f=open("data/scissors/scissors.txt",'r')
+        i=int(f.read())
+        f.close()
+        t=str(i)+'.jpg'
+        cv2.imwrite(os.path.join("data/scissors",t),frame)
+        f=open("data/scissors/scissors.txt",'w')
+        f.write("%d"%(i+1))
+        f.close()
+    elif c==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
---
+++ eyes.py
@@ -0,0 +1,41 @@
+# libraries
+import cv2
+import matplotlib as plt
+import numpy as np
+import keras
+from keras.models import load_model
+
+# load model
+model=load_model("data/traintemp.h5")
+
+# initialoize camera
+cam=cv2.VideoCapture(0)
+while(True):
+    
+#read frame and processing the frame
+    val,frame=cam.read()
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frame=frame[:,81:561]
+    cv2.imshow("CAMERA",frame)
+    frame=cv2.resize(frame,(64,64))
+    frame=frame.reshape(1,64,64,1)
+    
+#prediction
+    pred=model.predict(frame)
+    pred=pred[0]
+    pred.tolist()
+    m=max(pred)
+    if(pred[0]==m):
+        print("Rock")
+    elif(pred[1]==m):
+        print("Paper")
+    elif(pred[2]==m):
+		print("Scisors")
+    
+#release video object
+    c=cv2.waitKey(1)
+    if(c==ord('q')):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
